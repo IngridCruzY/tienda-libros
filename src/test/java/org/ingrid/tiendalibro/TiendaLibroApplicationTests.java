ed6ef91475e5c521271e898faa078936a6917b8d
Ingrid Lizbeth Cruz Ybañez <ingrid2209c@gmail.com>
2024-08-28 11:56:49 -0500

    creando proyecto

---
+++ src/test/java/org/ingrid/tiendalibro/TiendaLibroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ingrid.tiendalibro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaLibroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
